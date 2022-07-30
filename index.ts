e12b940189f68fa4ede496b22c733ece91e3bdcb
M. Gschwandtner <84477901+OnlyPain-ctrl@users.noreply.github.com>
2022-07-30 19:46:40 +0200

    added cors; added author

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -8,7 +9,12 @@ import apiRouter from './server/api'
 
 const app: Express = express()
 const port = process.env.PORT
-const rootDir = path.resolve(__dirname, '../../dist');
+const rootDir = path.resolve(__dirname, '../../dist')
+
+if (process.env.CORS == "true") {
+    console.log("cors on");
+    app.use(cors())
+}
 
 app.use('/', express.static(path.join(rootDir, 'vite')))
 app.use('/api', apiRouter)
